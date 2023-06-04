1db73c7a706985cf610a97ec460fdb3957ad3cd1
hideshimaryo <ryoh56762@gmail.com>
2023-06-04 13:27:41 +0900

    最初の状態

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+$(function(){
+	$("#drawer_toggle").click(function(){
+    $(this).toggleClass("open");
+    $('body').toggleClass("open");
+    $("#global_nav").toggleClass("sp_open");
+  });
+});
+
